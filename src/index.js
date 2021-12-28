f3fcbb7783e462b210545d86f20393e6fd223962
marisayumg <35278286+marisayumg@users.noreply.github.com>
2021-12-28 10:21:21 +0000

    Added the HTML and some CSS styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello");
+// get day and time
+
+let currentTime = new Date();
+let year = currentTime.getUTCFullYear();
+let hours = currentTime.getHours();
+
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let timeElement = document.querySelector("#time");
+timeElement.innerHTML = `Friday ${year}, ${hours}:${minutes} AM`;
